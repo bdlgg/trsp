c9a5bbbd3194560962fafec5c6b56c243ae6efa5
aleksejkonstantinov <40akka@gmail.com>
2026-02-26 15:46:50 +0300

    1_3 task

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 app = FastAPI()
+class Calculation(BaseModel):
+    num1: int
+    num2: int
 #1.1
 @app.get("/")
 async def root():
-    return{"message": "Добро пожаловать в мое приложение FastAPI!"}
+    return{"message": "Авторелоад реально работает"}
 
 #1.2
 @app.get("/pages")
@@ -12,8 +16,9 @@ async def root():
     return FileResponse("public/index.html")
 
 #1.3
-# @app.post("/calculate?num1=5&num2=10")
-# async def calculate(num1: int, num2: int):
-#     return{"result": 15}
+@app.post("/calculate")
+async def calculate(data: Calculation):
+    res = data.num1 + data.num2
+    return{"result": res}
 
 #1.4
